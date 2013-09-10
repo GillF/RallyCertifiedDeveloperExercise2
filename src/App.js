21c0c525d511d033d779bd8522a245249a27a8a1
Charles Ferentchak <ferentchak@gmail.com>
2013-09-10 10:41:32 -0600

    Solution

--- src/App.js
+++ src/App.js
@@ -2,18 +2,9 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     items:{
-        html:'<a href="https://help.rallydev.com/apps/2.0rc1/doc/#!/api/Rally.data.WsapiDataStore">Hint!<a>'
-    },
-    fireFromTheStore:function(store,records){
-        var blockedRecords = _.filter(records,function(record){
-            var blocked = record.get('Blocked')
-            console.log(blocked,record);
-            return blocked;
-        }); 
-        Ext.Msg.alert('Status', 'Store Loaded with '+records.length+' records and '+blockedRecords.length + ' blocked records.');
+        html:''
     },
 
     launch: function() {
-
     }
 });
